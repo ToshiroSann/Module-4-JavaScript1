566c867d3426919e79eb21c569d96ddf9e5009ab
ToshiroSann <owenhomanlaw@gmail.com>
2022-12-06 11:02:55 +0100

    first commit

---
+++ workshop-1/js/script.js
@@ -0,0 +1,46 @@
+var eersteVariabele = "test";
+console.log(eersteVariabele);
+
+
+
+var voornaam = "Owen ";
+var tussenvoegsel = ""; // wijzig dit in jouw eigen eventuele tussenvoegsel
+var achternaam = "Law";
+var volledigeNaam = voornaam + tussenvoegsel + achternaam;
+
+document.write ("Mijn naam is:",volledigeNaam);
+
+
+var geboortedag = "3";
+var geboortemaand = "juni";
+var geboortejaar = "2006";
+var huidig_jaartal = "2022"
+var leeftijd = huidig_jaartal -= geboortejaar;
+
+document.write("<br>")
+document.write ("Mijn leeftijd is:", leeftijd)
+
+
+var dag_van_het_jaar = 0; // bereken hier hoeveelste dag van dit kalenderjaar het is
+
+document.write("<br>")
+document.write ("dag van het jaar", )
+
+
+var straat = ""; // je straat;
+var huisnummer = ""; // je huisnummer;
+var woonplaats = ""; // je woonplaats;
+var woonachtig_in_utrecht = ""; // check hier dmv een boolean of jouw woonplaats gelijk is aan "Utrecht"
+
+
+var aantal_studenten_in_deze_klas = 0;
+var kosten_van_frikandelbroodje = 1.76;
+var totale_lunchkosten = 0; // bereken hier het totaalbedrag als iedere student 2 frikandelbroodjes zou kopen
+
+/* toon de waardes hieronder op het scherm gescheiden door het html: '<br>' element voor een nieuwe-regel
+  bijvoorbeeld:
+  document.write("Voornaam: " + voornaam);
+  document.write("<br>");
+  document.write("Achternaam: " + achternaam);
+  enzovoorts, zorg dat alle bovenstaande variabelen op het scherm komen
+*/
