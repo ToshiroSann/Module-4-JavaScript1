aff9ab6e58077631bb3742060d3e38be24b56fca
ToshiroSann <owenhomanlaw@gmail.com>
2022-12-09 12:34:36 +0100

    Done

--- workshop-1/js/script.js
+++ workshop-1/js/script.js
@@ -1,41 +1,53 @@
-var eersteVariabele = "test";
 console.log(eersteVariabele);
 
-
-
-var voornaam = "Owen ";
-var tussenvoegsel = ""; // wijzig dit in jouw eigen eventuele tussenvoegsel
-var achternaam = "Law";
+// plak hier de 3 bovenstaande variabelen aan elkaar, met spaties
+var voornaam = "Owen "; // wijzig dit in jouw eigen voornaam
+var tussenvoegsel = " "; // wijzig dit in jouw eigen eventuele tussenvoegsel
+var achternaam = "Law"; // wijzig dit in jouw eigen achternaam
 var volledigeNaam = voornaam + tussenvoegsel + achternaam;
 
-document.write ("Mijn naam is:",volledigeNaam);
+document.write("Mijn naam is: ", volledigeNaam);
 
-
-var geboortedag = "3";
-var geboortemaand = "juni";
-var geboortejaar = "2006";
-var huidig_jaartal = "2022"
+// bereken hier jouw leeftijd op basis van de eerder ingevulde geboortegegevens.
+var geboortedag = "3"; // je geboortedag
+var geboortemaand = "Juni"; // je geboortemaand
+var geboortejaar = "2006"; // je geboortejaar
+var huidig_jaartal = "2022"// het huidige jaar;
 var leeftijd = huidig_jaartal -= geboortejaar;
 
 document.write("<br>")
-document.write ("Mijn leeftijd is:", leeftijd)
+document.write("Mijn leeftijd is: ", leeftijd);
+
+// bereken hier hoeveelste dag van dit kalenderjaar het is
+var dag_van_het_jaar = 0;
+
+function myFunction(){
+        // check hier dmv een boolean of jouw woonplaats gelijk is aan "Utrecht"
+    
+        var straat = "Porta Basilica"; // je straat;
+        var huisnummer = "29"; // je huisnummer;
+        var woonplaats = "Houten"; // je woonplaats;
+        var woonachtig_in_utrecht = "Utrecht";
+    
+        if (woonplaats === woonachtig_in_utrecht) {
+            document.write("<br>");
+            document.write("Woont redelijk in de buurt ");
+        } else {
+            document.write("<br>");
+            document.write("Woont niet in de buurt van Utrecht");
+        }
+}
 
 
-var dag_van_het_jaar = 0; // bereken hier hoeveelste dag van dit kalenderjaar het is
+// bereken hier het totaalbedrag als iedere student 2 frikandelbroodjes zou kopen
+var aantal_studenten_in_deze_klas = 23;
+var kosten_van_frikandelbroodje = 1.76;
+var totale_lunchkosten = aantal_studenten_in_deze_klas *= kosten_van_frikandelbroodje *= 2;
 
 document.write("<br>")
-document.write ("dag van het jaar", )
+document.write("Totale lunch kosten: ", totale_lunchkosten);
 
-
-var straat = ""; // je straat;
-var huisnummer = ""; // je huisnummer;
-var woonplaats = ""; // je woonplaats;
-var woonachtig_in_utrecht = ""; // check hier dmv een boolean of jouw woonplaats gelijk is aan "Utrecht"
-
-
-var aantal_studenten_in_deze_klas = 0;
-var kosten_van_frikandelbroodje = 1.76;
-var totale_lunchkosten = 0; // bereken hier het totaalbedrag als iedere student 2 frikandelbroodjes zou kopen
+var eersteVariabele = "test";
 
 /* toon de waardes hieronder op het scherm gescheiden door het html: '<br>' element voor een nieuwe-regel
   bijvoorbeeld:
@@ -44,3 +56,5 @@ var totale_lunchkosten = 0; // bereken hier het totaalbedrag als iedere student 
   document.write("Achternaam: " + achternaam);
   enzovoorts, zorg dat alle bovenstaande variabelen op het scherm komen
 */
+
+myFunction()
